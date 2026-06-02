cfd08da2862738696316e2fd868b4e582ec6b537
isaewSergey <sergey@bulkagames.com>
2026-06-02 15:01:41 +0300

    1.0.1

--- Build/PussyIslandRestoreWeb.loader.js
+++ Build/PussyIslandRestoreWeb.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 89385130,
+    wasmFileSize: 89387178,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
